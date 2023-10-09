278986eeaf3837e0ea9e859883696f4b233a0269
sunchoi22 <sunchoi22@pos.idserve.net>
2023-10-09 23:55:18 +0900

    [ADD] resume 작업 폴더 업로드

---
+++ js/projects_app.js
@@ -0,0 +1,24 @@
+let cardHTML = "";
+let elements = [];
+console.log('이건됨');
+$.getJSON("../data/projects_app.json", function (data) {
+  $.each(data, function (i, item) {
+    cardHTML = `
+    <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+      <div class="card-body p-0">
+          <div class="d-md-flex align-items-center">
+              <div class="p-5">
+                  <h2 class="fw-bolder">${item.title}</h2>
+                  <p>${item.text}</p>
+                  <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="${item.href}">바로가기</a>
+              </div>
+              <img class="img-fluid float-right" src="${item.image}"
+                  alt="${item.title}" />
+          </div>
+      </div>
+  </div>
+`;
+    elements.push(cardHTML);
+  });
+  $(".projects-card-body").append(elements);
+});
