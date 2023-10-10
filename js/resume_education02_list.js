c54c7b0030ba9ca044d37c3c643672cdf5d2dec7
sunchoi22 <sunchoi22@pos.idserve.net>
2023-10-11 03:58:45 +0900

    [ADD] resume.html 제이슨으로 분류하여 정보 입력

--- js/resume_education02_list.js
+++ js/resume_education02_list.js
@@ -4,7 +4,11 @@ $.getJSON("./data/resume_education02_list.json", function (data) {
   // console.log(data);
   $.each(data, function (i, item) {
     ed02cardHTML = `
-    <h4 class="h6 fw-bolder m-0 mb-2"><a href="${item.href}" target="_blank">${item.title}</a></h4>
+    <h4 class="h6 fw-bolder m-0 mb-2">
+    <a href="${item.href}" target="_blank">${item.title}</a>
+    <a href="${item.href02}" target="_blank">[<i class="bi bi-github"></i> GitHub]</a>
+    <a href="${item.href03}" target="_blank">[<i class="bi bi-file-earmark-ppt-fill"></i> PT]</a>
+    </h4>
     <p class="mb-2">${item.text}</p>
     <h5 class="h6 fw-bolder m-0">개발 기간</h5>
     <p class="mb-2">${item.date}</p>
