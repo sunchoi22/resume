b0f889a75fe9068cc67202db46ea43b23e9cb9b1
sunchoi22 <sunchoi22@pos.idserve.net>
2023-10-18 18:20:04 +0900

    [ADD] 정보업데이트

--- js/resume_education02_list.js
+++ js/resume_education02_list.js
@@ -5,9 +5,10 @@ $.getJSON("./data/resume_education02_list.json", function (data) {
   $.each(data, function (i, item) {
     ed02cardHTML = `
     <h4 class="h6 fw-bolder m-0 mb-2">
-    <a href="${item.href}" target="_blank">${item.title}</a>
-    <a href="${item.href02}" target="_blank">[<i class="bi bi-github"></i> GitHub]</a>
-    <a href="${item.href03}" target="_blank">[<i class="bi bi-file-earmark-ppt-fill"></i> PT]</a>
+    <a href="${item.href_site}" target="_blank">${item.title}</a>
+    <a href="${item.href_github}" target="_blank">${item.icon_open}<i class="${item.bi_github_icon}"></i>${item.Github_text}${item.icon_close}</a>
+    <a href="${item.href_ppt}" target="_blank">${item.icon_open02}<i class="${item.bi_ppt_icon}"></i>${item.PT_text}${item.icon_close02}</a>
+    <a href="${item.href_design}" target="_blank">${item.icon_open03}<i class="${item.bi_design_icon}"></i>${item.design_text}${item.icon_close03}</a>
     </h4>
     <p class="mb-2">${item.text}</p>
     <h5 class="h6 fw-bolder m-0">개발 기간</h5>
