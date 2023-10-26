d55750fefb9e64e0bb3682347a746767c38d5237
sunchoi22 <sunchoi22@pos.idserve.net>
2023-10-26 17:29:58 +0900

    [Add] web 프로젝트 등록

--- js/projects_web.js
+++ js/projects_web.js
@@ -6,17 +6,19 @@ $.getJSON("../data/projects_web.json", function (data) {
     cardHTML = `
     <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
     <div class="projects-card-body card-body p-0">
+    <a href="${item.href}" target="_blank"><img class="img-fluid" src="${item.src}" alt="${item.title}"></a>
         <div class="d-md-flex align-items-center">
             <div class="p-5">
-                <h2 class="fw-bolder">${item.title}</h2>
+                <h2 class="fw-bolder h5">${item.title}</h2>
                 <p>${item.text}</p>
-                <p><b>개발 기간</b>${item.period}</p>
-                <p><b>개발 인원</b>${item.personnel}</p>
-                <p><b>담당 업무</b>${item.work}</p>
-                <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="${item.href}">바로가기</a>
+                <h3 class="fw-bolder h6">개발 기간</h3>
+                <p>${item.period}</p>
+                <h3 class="fw-bolder h6">개발 인원</h3>
+                <p>${item.personnel}</p>
+                <h3 class="fw-bolder h6">담당 업무</h3>
+                <p>${item.work}</p>
             </div>
-            <img class="img-fluid" src="${item.src}" alt="${item.title}">
-        </div>
+            </div>
     </div>
 </div>
     `;
